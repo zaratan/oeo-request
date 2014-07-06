8df074b1c3e513433a6d08e8ac06e59d81713083
Duy Le <masaruduy@gmail.com>
2014-07-06 16:18:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OeoRequest::Application.config.secret_key_base = '451d9061ff17df1db53744ed25eec47ed92d8357f21fc2af4a4d201452f976ee51fdeba3f0c1c73bf37a7770a2ec762b969dc53afa523c45d669c6d462a7d3d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OeoRequest::Application.config.session_store :cookie_store, key: '_oeo-request_session'
